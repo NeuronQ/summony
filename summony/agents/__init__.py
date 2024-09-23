bc63176610c05d53bfcfeb48fad7c982546e5f41
Andrei Anton <io@neuronq.ro>
2024-09-23 23:03:16 +0200

    Dummy agent

--- summony/agents/__init__.py
+++ summony/agents/__init__.py
@@ -2,5 +2,6 @@ from .agents import AgentInterface, Message
 
 from .openai_agent import OpenAIAgent
 from .anthropic_agent import AnthropicAgent
+from .dummy_agent import DummyAgent
 
 from .factory import get_default_agent_for_model
--- summony/agents/factory.py
+++ summony/agents/factory.py
@@ -1,9 +1,12 @@
 from .agents import AgentInterface
 from .anthropic_agent import AnthropicAgent
 from .openai_agent import OpenAIAgent
+from .dummy_agent import DummyAgent
 
 def get_default_agent_for_model(model: str) -> AgentInterface:
-    if model.startswith("gpt-"):
+    if model.startswith("dummy"):
+        return DummyAgent(model_name=model)
+    elif model.startswith("gpt-"):
         return OpenAIAgent(model_name=model)
     elif model.startswith("claude"):
         return AnthropicAgent(model_name=model)
